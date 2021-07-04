aa13eec8118ac38e0300a2fa552cabe61eb6064d
TheDingodile <jakob@3esser.dk>
2021-07-04 03:27:37 +0200

    123

--- Utils/experiments.sh
+++ Utils/experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mkdir ../outputs/Server_test2/
-mkdir ../outputs/Server_test2/Markdown
-bsub -o "../outputs/Server_test2/Markdown/Server_test2_0.md" -J "Server_test2_0" -env MYARGS="-name Server_test2-0 -hours 10 -num 0" < submit_gpu.sh
+mkdir ../outputs/Server_test3/
+mkdir ../outputs/Server_test3/Markdown
+bsub -o "../outputs/Server_test3/Markdown/Server_test3_0.md" -J "Server_test3_0" -env MYARGS="-name Server_test3-0 -hours 10 -num 0" < submit_gpu.sh
